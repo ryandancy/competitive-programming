4fcd77732c7076b51d355e0072f0102dbfe820bc
Ryan Dancy <ryan@keal.ca>
2019-08-05 20:42:25 -0400

    Add solution to 71A - Way Too Long Words

---
+++ codeforces/71A.cpp
@@ -0,0 +1,21 @@
+// 71A - Way Too Long Words
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  string s;
+  for (int i = 0; i < n; i++) {
+    cin >> s;
+    if (s.length() <= 10) {
+      cout << s << endl;
+    } else {
+      cout << s[0] << s.length() - 2 << s[s.length() - 1] << endl;
+    }
+  }
+}
