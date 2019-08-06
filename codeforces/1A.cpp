2ae9871ee8db25d345543859a77cbf9020f1f5d0
Ryan Dancy <ryan@keal.ca>
2019-08-05 20:28:11 -0400

    Add solution to Codeforces 1A

---
+++ codeforces/1A.cpp
@@ -0,0 +1,11 @@
+// 1A - Theatre Square
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long n, m, a;
+  cin >> n >> m >> a;
+  cout << (n/a + (n%a != 0)) * (m/a + (m%a != 0)) << endl;
+}
