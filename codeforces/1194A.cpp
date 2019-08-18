e7d027a40c88c6aeb73606537797a4bd4c3e0a9b
Ryan Dancy <ryan@keal.ca>
2019-08-18 09:01:12 -0400

    Add solution to 1194A - Remove a Progression

---
+++ codeforces/1194A.cpp
@@ -0,0 +1,17 @@
+// 1194A - Remove a Progression
+// The algorithm just removes all the odd numbers. The answer is 2*x.
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while (T--) {
+      int n, x;
+      cin >> n >> x;
+      cout << 2*x << endl;
+    }
+}
