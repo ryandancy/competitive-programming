f3865a22170508da385b52161f57f75fe86b27fb
Ryan Dancy <ryan@keal.ca>
2019-10-08 13:40:06 -0400

    Make 1238 solutions compile in codeforces dir

--- codeforces/1238A.cpp
+++ codeforces/1238A.cpp
@@ -2,15 +2,6 @@
 // Simple - any positive integer 1 has a prime factorization
 // Written during Codeforces Educational Round #74
 #include <bits/stdc++.h>
-#define MOD 1000000007
-#define INF 1000000000
-#define EPS 1e-9
-#define PI 3.1415926535897932384626433832795L
-#ifdef ONLINE_JUDGE
-#define debug(args...)
-#else
-#include "debug.h"
-#endif
 using namespace std;
 typedef long long ll;
 typedef long double ld;
--- codeforces/1238D.cpp
+++ codeforces/1238D.cpp
@@ -3,15 +3,6 @@
 // so we calculate the total number and then subtract those.
 // Written during Codeforces Educational Round #74
 #include <bits/stdc++.h>
-#define MOD 1000000007
-#define INF 1000000000
-#define EPS 1e-9
-#define PI 3.1415926535897932384626433832795L
-#ifdef ONLINE_JUDGE
-#define debug(args...)
-#else
-#include "debug.h"
-#endif
 using namespace std;
 typedef long long ll;
 typedef long double ld;
