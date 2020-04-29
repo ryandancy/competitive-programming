1c9838b07a1e9110972274fdc41e3d53bc3c59f2
Ryan Dancy <ryan@keal.ca>
2020-04-29 13:44:37 -0400

    Add solution to CSES Range Minimum Queries II

--- cses/rmq-single-update-seg-tree.cpp
+++ cses/rmq-single-update-seg-tree.cpp
@@ -1,5 +1,5 @@
-// Single update + range sum queries with a segment tree in O(n log n + q log n).
-// Can also be done with a BIT.
+// Single update + range min queries with a segment tree in O(n log n + q log n).
+// CAN'T actually be done (as easily) with a BIT.
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,12 +8,12 @@ typedef long long ll;
 ll st[4000001]; // double size + 1
 int n, o, q;
 
-ll sum(int a, int b) {
+ll get_min(int a, int b) {
   a += o, b += o;
-  ll res = 0;
+  ll res = st[a];
   while (a <= b) {
-    if (a%2 == 1) res += st[a++];
-    if (b%2 == 0) res += st[b--];
+    if (a%2 == 1) res = min(res, st[a++]);
+    if (b%2 == 0) res = min(res, st[b--]);
     a /= 2, b /= 2;
   }
   return res;
@@ -21,10 +21,10 @@ ll sum(int a, int b) {
 
 void update(int a, ll v) {
   a += o;
-  ll old = st[a];
+  st[a] = v;
+  a /= 2;
   while (a > 0) {
-    st[a] -= old;
-    st[a] += v;
+    st[a] = min(st[2*a], st[2*a+1]);
     a /= 2;
   }
 }
@@ -39,13 +39,14 @@ int main() {
   o=1;
   while (o<n) o*=2;
   
-  // input the array in the leaves (they are their own sum)
+  // input the array in the leaves (they are their own min)
+  // we *could* use update here to skip the building part
   for (int i = 0; i < n; i++) cin>>st[o+i];
   
-  // build the rest of the segment tree (combine=sum)
+  // build the rest of the segment tree (combine=min)
   for (int i = o/2; i > 0; i /= 2) {
     for (int j = i; j < 2*i; j++) {
-      st[j] = st[2*j] + st[2*j+1];
+      st[j] = min(st[2*j], st[2*j+1]);
     }
   }
   
@@ -56,7 +57,7 @@ int main() {
     if (t==1) {
       update(a-1, b);
     } else {
-      cout<<sum(a-1, b-1)<<'\n';
+      cout<<get_min(a-1, b-1)<<'\n';
     }
   }
 }
